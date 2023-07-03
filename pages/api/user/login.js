60b05e2ab1d2de258aa76010c1509730be4cb493
Gabriel Costa <22gabrielgc@gmail.com>
2023-07-03 17:05:56 -0300

    Criado serviço de autenticação

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -1,8 +1,10 @@
 import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
 
 import databaseHandler from '../../../lib/middlewares/nextConnect'
 import validation from '../../../lib/middlewares/validation'
 import { loginUser } from '../../../modules/user/user.service'
+import { ironConfig } from '../../../lib/middlewares/ironSession'
 
 const loginSchema = Joi.object({
   //Função criada para validar se os dados do usuario estão corretos antes deste ser criado no banco de dados
@@ -10,14 +12,19 @@ const loginSchema = Joi.object({
   password: Joi.string().required()
 })
 
-const signup = databaseHandler().post(validation({ body: loginSchema }), async (req, res) => {
+const login = databaseHandler().post(validation({ body: loginSchema }), async (req, res) => {
   try {
     const user = await loginUser(req.body)
-    res.send(user)
+    req.session.user = {
+      id: user._id,
+      user: user.user
+    }
+    await req.session.save()
+    res.send({ loginOk: true })
   } catch (error) {
     console.error(error)
     throw error
   }
 })
 
-export default signup
+export default withIronSessionApiRoute(login, ironConfig)
--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,8 +1,10 @@
 import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
 
 import databaseHandler from '../../../lib/middlewares/nextConnect'
 import validation from '../../../lib/middlewares/validation'
 import { signupUser } from '../../../modules/user/user.service'
+import { ironConfig } from '../../../lib/middlewares/ironSession'
 
 const userSchema = Joi.object({
   //Função criada para validar se os dados do usuario estão corretos antes deste ser criado no banco de dados
@@ -14,11 +16,16 @@ const userSchema = Joi.object({
 const signup = databaseHandler().post(validation({ body: userSchema }), async (req, res) => {
   try {
     const user = await signupUser(req.body)
-    res.status(201).json(user)
+    req.session.user = {
+      id: user._id,
+      user: user.user
+    }
+    await req.session.save()
+    res.status(201).json( { signupOk: true })
   } catch (error) {
     console.error(error)
     throw error
   }
 })
 
-export default signup
+export default withIronSessionApiRoute(signup, ironConfig)
